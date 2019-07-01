adf8b13cce99f90092f1eadd877fbff8fbc82f74
jiangtaisheng <949431495@qq.com>
2019-07-01 19:51:03 +0800

    22
    44

--- Photodisplay/AppDelegate.m
+++ Photodisplay/AppDelegate.m
@@ -17,8 +17,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    
-    
+    // Override point for customization after application launch.
+    // Override point for customization after application launch.
     self.window=[[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
     self.window.backgroundColor=[UIColor whiteColor];
     
